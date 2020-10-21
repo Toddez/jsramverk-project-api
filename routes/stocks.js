5c20479f892a8419918002ab84603feb0b037500
Teo Carlsson <carlsson.teo@gmail.com>
2020-10-21 16:05:36 +0200

    Added way to get information of a specific stock

--- routes/stocks.js
+++ routes/stocks.js
@@ -2,17 +2,47 @@ const express = require('express');
 const router = express.Router();
 
 const db = require('../db/database.js');
+const ObjectId = require('mongodb').ObjectID;
 
 router.get('/', (req, res, next) => {
     const collection = db.client().db('project').collection('stocks');
 
-    collection.findMany({}, (err, stocks) => {
+    collection.find().toArray((err, stocks) => {
         if (err) {
             return next(new Error('Database error'));
         }
 
-        // TODO: Filter result
-        res.status(200).json(stocks);
+        let data = stocks.map((stock) => {
+            return {
+                id: stock._id,
+                name: stock.name,
+                value: stock.value,
+                current: stock.value[stock.value.length - 1]
+            };
+        });
+
+        data = data.sort((a, b) => a.current > b.current);
+
+        res.status(200).json({ stocks: data });
+    });
+});
+
+router.get('/:id', (req, res, next) => {
+    const collection = db.client().db('project').collection('stocks');
+
+    collection.findOne({ _id: new ObjectId(req.params.id) }, (err, stock) => {
+        if (err) {
+            return next(new Error('Database error'));
+        }
+
+        let data = {
+            id: stock._id,
+            name: stock.name,
+            value: stock.value,
+            current: stock.value[stock.value.length -1]
+        };
+
+        res.status(200).json(data);
     });
 });
 
