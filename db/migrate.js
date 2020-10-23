291bb165a9d13673d90f16057eef393d1ae666ce
Teo Carlsson <carlsson.teo@gmail.com>
2020-10-23 17:24:18 +0200

    Added more default stocks

--- db/migrate.js
+++ db/migrate.js
@@ -19,6 +19,54 @@ const { Database, db } = require('./database.js');
             value: [
                 { date: '2020-10-22T11:22:04.281Z', value: 100 }
             ]
+        },
+        {
+            name: 'Nvidia',
+            value: [
+                { date: '2020-10-22T11:22:04.281Z', value: 100 }
+            ]
+        },
+        {
+            name: 'MSI',
+            value: [
+                { date: '2020-10-22T11:22:04.281Z', value: 100 }
+            ]
+        },
+        {
+            name: 'NZXT',
+            value: [
+                { date: '2020-10-22T11:22:04.281Z', value: 100 }
+            ]
+        },
+        {
+            name: 'Corsair',
+            value: [
+                { date: '2020-10-22T11:22:04.281Z', value: 100 }
+            ]
+        },
+        {
+            name: 'EVGA',
+            value: [
+                { date: '2020-10-22T11:22:04.281Z', value: 100 }
+            ]
+        },
+        {
+            name: 'ASUS',
+            value: [
+                { date: '2020-10-22T11:22:04.281Z', value: 100 }
+            ]
+        },
+        {
+            name: 'ASROCK',
+            value: [
+                { date: '2020-10-22T11:22:04.281Z', value: 100 }
+            ]
+        },
+        {
+            name: 'Dell',
+            value: [
+                { date: '2020-10-22T11:22:04.281Z', value: 100 }
+            ]
         }
     ], (err) => {
         if (err) {
