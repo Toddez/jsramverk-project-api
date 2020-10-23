3cffc708906dd08d6f9a37df428b9852150225e3
Teo Carlsson <carlsson.teo@gmail.com>
2020-10-23 17:25:01 +0200

    Added temporary db clients

--- db/database.js
+++ db/database.js
@@ -2,6 +2,17 @@ const mongodb = require('mongodb');
 
 const url = 'mongodb://localhost:27017';
 
+class Client {
+    constructor(client) {
+        this.client = client;
+    }
+
+    close() {
+        const client = Database.clients.pop();
+        client.client.close();
+    }
+}
+
 class Database {
     static async connect() {
         await new Promise(resolve => {
@@ -23,13 +34,45 @@ class Database {
         return Database;
     }
 
-    static client() {
-        if (!Database.mongoClient)
-            throw new Error('Databasen körs inte');
+    static async temporaryClient() {
+        let newClient;
+        await new Promise(resolve => {
+            mongodb.MongoClient.connect(
+                url,
+                {
+                    useUnifiedTopology: true
+                },
+                (err, client) => {
+                    if (err)
+                        return;
 
-        return Database.mongoClient;
+                    newClient = client;
+                    resolve();
+                }
+            );
+        });
+
+        return new Client(newClient);
+    }
+
+    static use(client) {
+        Database.clients.push(client);
+    }
+
+    static client() {
+        if (Database.mongoClient) {
+            return Database.mongoClient;
+        }
+
+        if (Database.clients.length > 0) {
+            return Database.clients[0].client;
+        }
+
+        throw new Error('Databasen körs inte');
     }
 }
+
+Database.clients = [];
 
 let db = 'project';
 if (process.env.NODE_ENV === 'test') {
