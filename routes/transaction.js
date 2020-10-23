7e95d9fc8387e8d873bf3db8b16094c963c564c7
Teo Carlsson <carlsson.teo@gmail.com>
2020-10-23 15:37:49 +0200

    Added the ability to buy and sell stocks

--- routes/transaction.js
+++ routes/transaction.js
@@ -2,21 +2,163 @@ const express = require('express');
 const router = express.Router();
 
 const db = require('../db/database.js');
+const ObjectId = require('mongodb').ObjectID;
 
 router.get('/inventory', (req, res, next) => {
-    const collection = db.client().db('project').collection('users');
+    const users = db.client().db('project').collection('users');
+    const stocksCollection = db.client().db('project').collection('stocks');
 
-    collection.findOne({email: req.user.email}, (err, user) => {
+    users.findOne({email: req.user.email}, (err, user) => {
         if (err) {
             return next(new Error('Database error'));
         }
 
-        const data = {
-            balance: user.balance || 0,
-            inventory: user.inventory || []
-        };
+        stocksCollection.find({}).toArray((err, stocks) => {
+            if (err) {
+                console.log(err);
+                return next(new Error('Database error'));
+            }
 
-        res.status(200).json(data);
+            let inventoryData = [];
+
+            for (const id in user.inventory) {
+                if (user.inventory[id]) {
+                    const item = user.inventory[id];
+
+                    const stock = stocks.find((value) => {
+                        return value._id == id;
+                    });
+
+                    inventoryData.push({
+                        id: id,
+                        amount: item.amount,
+                        name: stock.name,
+                        value: stock.value[stock.value.length -1].value
+                    });
+                }
+            }
+
+            const data = {
+                balance: user.balance || 0,
+                inventory: inventoryData || {}
+            };
+
+            res.status(200).json(data);
+        });
+    });
+});
+
+router.post('/buy', (req, res, next) => {
+    const users = db.client().db('project').collection('users');
+    const stocks = db.client().db('project').collection('stocks');
+
+    if (req.body.amount <= 0 || !Number.isInteger(parseFloat(req.body.amount))) {
+        return next(new Error('Amount must be a positive integer'));
+    }
+
+    users.findOne({email: req.user.email}, (err, user) => {
+        if (err) {
+            return next(new Error('Database error'));
+        }
+
+        stocks.findOne({_id: ObjectId(req.body.id)}, (err, stock) => {
+            if (err) {
+                return next(new Error('Database error'));
+            }
+
+            const stockValue = stock.value[stock.value.length - 1].value;
+
+            if (stockValue * req.body.amount > user.balance) {
+                return next(new Error('Not enough balance'));
+            }
+
+            let currentAmount = 0;
+            if (user.inventory[ObjectId(req.body.id)]) {
+                currentAmount = user.inventory[ObjectId(req.body.id)].amount;
+            }
+
+            const newBalance = user.balance - stockValue * req.body.amount;
+
+            users.updateOne({email: req.user.email}, {
+                $set: {
+                    balance: newBalance,
+                    inventory: {
+                        ...user.inventory,
+                        [ObjectId(req.body.id)]: {
+                            amount: currentAmount + parseInt(req.body.amount)
+                        }
+                    }
+                }
+            }, (err) => {
+                if (err) {
+                    return next(new Error('Database error'));
+                }
+
+                res.status(201).json({
+                    balance: newBalance,
+                    amount: currentAmount + parseInt(req.body.amount),
+                    message: 'Successfully bought stocks'
+                });
+            });
+        });
+
+    });
+});
+
+router.post('/sell', (req, res, next) => {
+    const users = db.client().db('project').collection('users');
+    const stocks = db.client().db('project').collection('stocks');
+
+    if (req.body.amount <= 0 || !Number.isInteger(parseFloat(req.body.amount))) {
+        return next(new Error('Amount must be a positive integer'));
+    }
+
+    users.findOne({email: req.user.email}, (err, user) => {
+        if (err) {
+            return next(new Error('Database error'));
+        }
+
+        stocks.findOne({_id: ObjectId(req.body.id)}, (err, stock) => {
+            if (err) {
+                return next(new Error('Database error'));
+            }
+
+            const stockValue = stock.value[stock.value.length - 1].value;
+
+            let currentAmount = 0;
+            if (user.inventory[ObjectId(req.body.id)]) {
+                currentAmount = user.inventory[ObjectId(req.body.id)].amount;
+            }
+
+            if (currentAmount < req.body.amount ) {
+                return next(new Error('Not enough stocks'));
+            }
+
+            const newBalance = user.balance + stockValue * req.body.amount;
+
+            users.updateOne({email: req.user.email}, {
+                $set: {
+                    balance: newBalance,
+                    inventory: {
+                        ...user.inventory,
+                        [ObjectId(req.body.id)]: {
+                            amount: currentAmount - parseInt(req.body.amount)
+                        }
+                    }
+                }
+            }, (err) => {
+                if (err) {
+                    return next(new Error('Database error'));
+                }
+
+                res.status(201).json({
+                    balance: newBalance,
+                    amount: currentAmount - parseInt(req.body.amount),
+                    message: 'Successfully sold stocks'
+                });
+            });
+        });
+
     });
 });
 
